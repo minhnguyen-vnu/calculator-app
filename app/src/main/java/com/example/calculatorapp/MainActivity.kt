bc109e838bc4b6583265f3c5d17510f51b6c8b66
minhnguyen <23020114@vnu.edu.vn>
2026-03-09 00:58:05 +0700

    initial commit

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.calculatorapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.calculatorapp.ui.theme.CalculatorAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CalculatorScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CalculatorScreen(modifier: Modifier = Modifier) {
+    // Column is like a vertical <div> in HTML or a vertical layout in Java Swing/AWT
+    Column(modifier = modifier.padding(15.dp)) {
+        Text(text = "Calculator Result: 0")
+
+        Button(onClick = {
+            Log.d("CalculatorApp", "Button was clicked!")
+        }) {
+            Text(text = "Click Me")
+        }
+    }
+}
